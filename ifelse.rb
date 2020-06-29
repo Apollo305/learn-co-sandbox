6bc417797fb106c11f4d05cce315f23b5501bff4
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-06-29 16:48:43 +0000

    Automatically backed up by Learn

---
+++ ifelse.rb
@@ -0,0 +1,13 @@
+if 1 + 1 == 2
+  puts "one plus one is two!"
+else
+  puts "one plus one is not two"
+end
+# this will print 5 is greater than two because its true
+if 5 > 2 
+  print "5 is greater than 2"
+end
+# this will print nothing because its false
+if 2 > 5
+  puts "2 is greater than 5"
+end
---
+++ looping.rb
@@ -0,0 +1,14 @@
+# this will print 10 times in the terminal
+#10.times do 
+ # puts "Looping is easier than writing it multipe times"
+#end
+#infinate Loop
+#loop do
+ # puts "I have found the Time Machine!"
+#end
+#while loop
+counter = 0
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1
+end
